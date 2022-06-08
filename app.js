b0eb5fd3b3eb3c0c35baee5d4c556d706ca0bb6f
asknksk <asknkskk@gmail.com>
2022-06-08 12:31:59 +0300

    selected elemen on js

--- app.js
+++ app.js
@@ -2,5 +2,25 @@ let buttons = document.querySelector(".buttons");
 let screen = document.querySelector(".screen");
 
 buttons.addEventListener("click", (event) => {
-  console.log(event.target);
+  //   console.log(event.target.innerText);
+  //   console.log(
+  //     ["AC", "±", "%", ".", "="].every((e) => {
+  //       return e != event.target.innerText;
+  //     })
+  //   );
+  if (
+    ["AC", "C", "±", "%", ".", "="].every((e) => {
+      return e != event.target.innerText;
+    })
+  ) {
+    screen.innerText += event.target.innerText;
+    buttons.querySelector(".Ac").innerText = "AC";
+  } else if (event.target.innerText == "AC") {
+    buttons.querySelector(".Ac").innerText = "C";
+    reset();
+  }
 });
+
+function reset() {
+  screen.innerText = "";
+}
