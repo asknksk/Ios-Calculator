7f660bf0c37109a2847800ac49def76858cfaf84
asknksk <asknkskk@gmail.com>
2022-06-08 11:07:01 +0300

    app.js added

---
+++ app.js
@@ -0,0 +1,6 @@
+let buttons = document.querySelector(".buttons");
+let screen = document.querySelector(".screen");
+
+buttons.addEventListener("click", (event) => {
+  console.log(event.target);
+});
